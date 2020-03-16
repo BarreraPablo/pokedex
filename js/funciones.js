6f4ebd91eda2aa5c5d561bca92fa62bdabc06f60
BarreraPablo <55936076+BarreraPablo@users.noreply.github.com>
2020-03-16 19:32:59 -0300

    Eslint como devDependency y se ejecuta con 'standard'

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,12 @@
-import { mostrarPokemon , setearNombreEImagen, mostrarError } from './ui.js';
+import { mostrarPokemon, setearNombreEImagen, mostrarError } from './ui.js';
 
-
-export function cargarDefault(){
-	cargarPokemones(0);
-	$($('#pagination button')[0]).addClass('activo'); 
+function habilitarClick (cuadro, url) {
+	$(cuadro).on('click', () => {
+		mostrarPokemon(url);
+	});
 }
 
-export function cargarPokemones(offset){
+export function cargarPokemones (offset) {
 	const cartasPokemones = $('.row .centrado');
 	const saltoID = 807; // Se produce un salto en los ID, el siguiente al ID 807 es el ID 10.001
 	const diferencia = 9194; // Para conseguir el ID de los pokemones con ID mayor a 807 hay que sumarles 9.194 (806 -> 807 -> 10.001 -> 10.002)
@@ -14,16 +14,16 @@ export function cargarPokemones(offset){
 	$.ajax({
 		method: 'GET',
 		url: `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=48`,
-		success: respuesta => {
-			$(respuesta.results).each(function(index) {
+		success: (respuesta) => {
+			$(respuesta.results).each(function (index) {
 				let indice = 0;
-				if(offset + index + 1 > saltoID){
+				if (offset + index + 1 > saltoID) {
 					indice = offset + index + diferencia;
-				}else{
+				} else {
 					indice = offset + index + 1;
 				}
 
-				setearNombreEImagen($(cartasPokemones[index]) , this.name.toUpperCase() , `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${indice}.png`);
+				setearNombreEImagen($(cartasPokemones[index]), this.name.toUpperCase(), `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${indice}.png`);
 				habilitarClick(cartasPokemones[index], this.url);
 			});
 		}
@@ -32,19 +32,17 @@ export function cargarPokemones(offset){
 	});
 }
 
-function habilitarClick(cuadro, url){
-	$(cuadro).on('click', function(){
-		mostrarPokemon(url);
-	});
+export function cargarDefault () {
+	cargarPokemones(0);
+	$($('#pagination button')[0]).addClass('activo');
 }
 
-
-export function habilitarBotonSearch(){
-	$('#buscar').on('click', function(event){
+export function habilitarBotonSearch () {
+	$('#buscar').on('click', (event) => {
 		const pokemon = $('input[type=search]').val().toLocaleLowerCase();
 
 		mostrarPokemon(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
 		event.preventDefault();
-	});    
+	});
 }
--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-import { generarMatriz , generarNavegadorPaginas } from './ui.js';
-import { cargarDefault , habilitarBotonSearch } from './funciones.js';
+import { generarMatriz, generarNavegadorPaginas } from './ui.js';
+import { cargarDefault, habilitarBotonSearch } from './funciones.js';
 
-
-function inicializar(){
+function inicializar () {
 	generarMatriz();
 	generarNavegadorPaginas();
 	habilitarBotonSearch();
 	cargarDefault();
 }
-
 
 inicializar();
